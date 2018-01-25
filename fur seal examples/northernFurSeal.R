8e7de40b39c46b06f6c8700a2f808832e10fc37c
Devin <devin.johnson@noaa.gov>
2018-01-25 09:44:53 -0800

    update NFS example

--- fur seal examples/northernFurSeal.R
+++ fur seal examples/northernFurSeal.R
@@ -30,7 +30,7 @@ fit1 <- crwMLE(
   mov.model=~1, err.model=list(x=~Argos_loc_class-1), drift=TRUE,
   data=northernFurSeal, Time.name="Time", 
   initial.state=initial, fixPar=fixPar, constr=constr,# prior=ln.prior,
-  theta = c(rep(log(1500)+1,2), log(10), -3, 0,0),
+  theta = c(rep(log(1500)+1,2), log(3000), -3, 0,0),
   method="L-BFGS-B",
   control=list(trace=1, REPORT=1)
 )
