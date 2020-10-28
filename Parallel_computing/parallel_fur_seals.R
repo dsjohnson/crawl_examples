867333462e27bb54bb2b7bfadbde7402373dbf36
Devin <devin.johnson@noaa.gov>
2020-10-28 13:38:49 -0700

    update parallel fur seals

--- Parallel_computing/parallel_fur_seals.R
+++ Parallel_computing/parallel_fur_seals.R
@@ -2,14 +2,17 @@ library(tidyverse)
 library(sf)
 library(crawl)
 library(mapview)
-library(furrr)
+library(foreach)
+library(doFuture)
+
+registerDoFuture()
 
 
 ### Obtain fur seal data  
 download.file("https://ndownloader.figshare.com/files/10364766", "nfs_pups_2005.csv")
 
 ### Read in pup telemetry data
-pup_frame = read_csv("nfs_pups_2005.csv", 
+pup_frame <- read_csv("nfs_pups_2005.csv", 
                      col_types=cols(
                        dbid = col_integer(),
                        site = col_character(),
@@ -27,42 +30,43 @@ pup_frame = read_csv("nfs_pups_2005.csv",
   dplyr::select(dbid, site, sex, GMT, long, lat, loc_class) 
 
 ### Convert to sf object and project
-pup_frame = pup_frame %>% st_as_sf(coords=c("long","lat")) %>% 
+pup_frame <- pup_frame %>% st_as_sf(coords=c("long","lat")) %>% 
   st_set_crs(4326) %>% st_transform(3832)
 
 ### Split data by individual
-pup_frame = pup_frame %>% group_by(dbid, site, sex) %>% nest()
+pup_frame <- pup_frame %>% group_by(dbid, site, sex) %>% nest()
 
 
 ### Fit crawl model in parallel over animals
 ### Will be based on parallel R sessions 
-plan("multisession")
+plan("multisession", workers=6)
 
-pup_frame %>% 
-  mutate(
-    # Fit a ctcrw model and make some predictions
-    fit_pred = future_map(data, 
-                         .f=~{
-                           # fit ctcrw model
-                           fit = crwMLE(
-                             err.model=list(x=~loc_class-1), 
-                             drift=TRUE,
-                             data = .x,
-                             Time.name="GMT",
-                             fixPar=c(log(250), log(500), log(1500), NA, NA, NA,4, NA, NA),
-                             theta=c(log(2000), log(2000), 5, 0, 0),
-                             constr=list(
-                               lower=c(rep(log(1500),2), rep(-Inf,3)), upper=Inf
-                             ), method='L-BFGS-B', attempts = 5)
-                           # Make some predictions at 6 hour intervals
-                           pred = crwPredict(fit, predTime = "1 day") %>% 
-                             crw_as_tibble() %>% filter(locType=="p")
-                           list(fit=fit, pred=pred)
-                         }
-    ),
-    # Just bookeeping to split fit and pred apart
-    fit = map(fit_pred, ~.x$fit),
-    pred = map(fit_pred, ~.x$pred)
+pup_frame$fit_pred <- foreach(i=1:nrow(pup_frame)) %dopar% {
+      # fit ctcrw model
+      set.seed(123)
+      .x <- pup_frame$data[[i]]
+      if(nrow(.x)<20) return(NA)
+      fit <- crwMLE(
+        err.model=list(x=~loc_class-1), 
+        data = .x,
+        Time.name="GMT",
+        fixPar=c(log(250), log(500), log(1500), NA, NA, NA,NA),
+        theta=c(log(2000), log(2000), 8, -3),
+        constr=list(
+          lower=c(rep(log(1500),2), rep(-Inf,2)), upper=Inf
+        ), method='L-BFGS-B', attempts = 5)
+      # Make some predictions at 6 hour intervals
+      pred = crwPredict(fit, predTime = "1 day") %>% 
+        crw_as_tibble() %>% filter(locType=="p")
+      return(list(fit=fit, pred=pred))
+}
+
+plan("sequential")
+
+pup_frame <- pup_frame %>% filter(!is.na(fit_pred)) %>% mutate(
+# Just bookeeping to split fit and pred apart
+    fit = map(fit_pred, ~{.x$fit}),
+    pred = map(fit_pred, ~{.x$pred})
   ) %>% 
   select(-fit_pred) -> pup_frame
 
@@ -70,9 +74,9 @@ pup_frame %>%
 
 ### Make some plots
 # get shoreline 
-np = ptolemy::npac() # install with-- devtools::install_github("jmlondon/ptolemy")
+np <- ptolemy::npac() # install with-- devtools::install_github("jmlondon/ptolemy")
 
-pred_data = pup_frame %>% select(-fit) %>% unnest(pred)
+pred_data <- pup_frame %>% filter(!is.na(pred)) %>% select(-fit) %>% unnest(cols=pred)
 bb = c(range(pred_data$mu.x), range(pred_data$mu.y)) + c(-1, -1, 1, 1)*100000
 
 
